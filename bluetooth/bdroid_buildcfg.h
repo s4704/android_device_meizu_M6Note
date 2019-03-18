62817c69071ea335d32ace033c067c30f8c45faf
s4704 <igorkud00@gmail.com>
2019-03-18 19:47:25 +0300

    bluetooth: rename to Meizu M6 Note
    Change-Id: I6a36e121a4f3b73a839bc94350aed3ed87f7c6d9

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Smartisan U3"
+#define BTM_DEF_LOCAL_NAME   "Meizu M6 Note"
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
