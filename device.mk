5a58f50c610cb7a174a7cbf0d89ca788296264d0
s4704 <igorkud00@gmail.com>
2019-03-27 18:49:29 +0300

    lineage
    Change-Id: I70571f51c61738d47e8d521c07d7a09d2bde32f5

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/meizu/m1721/m1721-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mokee
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
--- lineage.mk
+++ lineage.mk
@@ -21,30 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from m1721 device
 $(call inherit-product, device/meizu/m1721/device.mk)
 
-# Inherit some common MK stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.mk.maintainer=XiNGRZ
-
-PRODUCT_NAME := mk_m1721
+PRODUCT_NAME := lineage_m1721
 PRODUCT_BRAND := meizu
 PRODUCT_DEVICE := m1721
 PRODUCT_MANUFACTURER := meizu
-PRODUCT_MODEL := OC105
+PRODUCT_MODEL := M6 Note
 
 TARGET_VENDOR_PRODUCT_NAME := m1721
 
 PRODUCT_GMS_CLIENTID_BASE := android-meizu
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="m1721-user 7.1.2 N2G47H 1 dev-keys"
-
-BUILD_FINGERPRINT := Android/m1721/m1721:7.1.2/N2G47H/1540887975:user/dev-keys
-
-# Sign bootable images
-PRODUCT_SUPPORTS_BOOT_SIGNER := true
-PRODUCT_SUPPORTS_VERITY := true
-PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/testkey
+    BUILD_FINGERPRINT="Meizu/meizu_M6Note/M6Note:7.1.2/N2G47H/m1721.Flyme_7.0.1539977208:user/release-keys" \
+    PRIVATE_BUILD_DESC="meizu_M6Note-user 7.1.2 N2G47H m1721.Flyme_7.0.1539977208 release-keys"
 
 TARGET_VENDOR := meizu
