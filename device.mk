f5d09e32889c823a621e529ed9230853d041e191
s4704 <igorkud00@gmail.com>
2019-04-07 20:35:44 +0300

    move camera_config.xml to device
    Change-Id: I22149df7f6c9f0af1f0389a079459b36656ddf5b

--- device.mk
+++ device.mk
@@ -157,6 +157,9 @@ PRODUCT_PACKAGES += \
     libbthost_if
 
 # Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/camera_config.xml:system/etc/camera/camera_config.xml
+
 PRODUCT_PACKAGES += \
     Snap
 
