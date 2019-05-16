2d8d3eeaa3aa3ab7624b0dc965fdacb27a18d540
s4704 <igorkud00@gmail.com>
2019-05-16 20:33:48 +0300

    overlay: combine with overlay-lineage
    Change-Id: I620b1fcdd94579b1674dc75171af27df0ee30b45

--- device.mk
+++ device.mk
@@ -24,9 +24,7 @@
 $(call inherit-product, vendor/meizu/m1721/m1721-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
