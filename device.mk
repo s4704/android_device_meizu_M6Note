8acd741906865823dd11c642e1c3596841577c67
s4704 <igorkud00@gmail.com>
2019-05-18 10:08:59 +0300

    Revert "keylayout: add custom Generic.kl for back button vibro"
    This reverts commit 4cce25db311c45a6b0b34231ff20e05a92dd243e.

--- device.mk
+++ device.mk
@@ -186,7 +186,6 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/keylayout/gf-keys.kl:system/usr/keylayout/gf-keys.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
